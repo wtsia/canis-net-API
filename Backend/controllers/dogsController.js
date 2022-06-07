438f8d6660759fdb50033b2fc0153a5b25eb96da
Winston T <54168818+wtsia@users.noreply.github.com>
2022-06-07 02:29:36 -0700

    dogs controller and routes

--- Backend/controllers/dogsController.js
+++ Backend/controllers/dogsController.js
@@ -1,36 +1,37 @@
+const asyncHandler = require('express-async-handler')
 
 // @desc    Get Dogs
 // @route   GET /api/dogs
 // @access  Private
-const getDogs = (req, res) => {
+const getDogs = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get Message' })
-};
+});
 
 // @desc    Set Dogs
 // @route   POST /api/dogs
 // @access  Private
-const setDog = (req, res) => {
+const setDog = asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400);
         throw new Error('Please add a text field');
     }
 
     res.status(200).json({ message: 'Set Dogs' })
-};
+});
 
 // @desc    Update Dogs
 // @route   PUT /api/dogs/:id
 // @access  Private
-const updateDog = (req, res) => {
+const updateDog = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update dog ${req.params.id}` })
-};
+});
 
 // @desc    Delete Dogs
 // @route   DELETE /api/dogs/:id
 // @access  Private
-const deleteDog = (req, res) => {
+const deleteDog = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete dog ${req.params.id}` })
-};
+});
 
 module.exports = {
     getDogs, 
